a5923b2eae1efc7106850fdf7d3b72fc8cd40940
Erich Ess <erich.g.ess@gmail.com>
2014-03-01 10:08:05 -0800

    Initial commit of a basic RabbitMQ example

---
+++ RabbitMQ/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open RabbitMQ.Client
+open System.Text
+
+[<EntryPoint>]
+let main argv = 
+    let factory = new ConnectionFactory(HostName = "localhost")
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+
+    channel.QueueDeclare( "hello", false, false, false, null )
+    let message = "Hello, World"
+    let body = Encoding.UTF8.GetBytes(message)
+
+    while true do
+        channel.BasicPublish("", "hello", null, body)
+        System.Threading.Thread.Sleep(2000)
+
+    printfn "%A" argv
+    0 // return an integer exit code
---
+++ Receiver/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System.Text
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+
+[<EntryPoint>]
+let main argv = 
+    let factory = new ConnectionFactory(HostName = "localhost")
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+
+    channel.QueueDeclare( "hello", false, false, false, null )
+    let consumer = new QueueingBasicConsumer(channel)
+    channel.BasicConsume("hello", true, consumer)
+
+    let queue = seq{
+                    while true do
+                        let ea = consumer.Queue.Dequeue() :> BasicDeliverEventArgs
+                        let body = ea.Body
+                        let message = Encoding.UTF8.GetString(body)
+                        yield message
+                }
+
+    queue |> Seq.map (fun s -> s.ToUpper() ) |> Seq.iter (printfn "%s")
+
+    printfn "%A" argv
+    0 // return an integer exit code

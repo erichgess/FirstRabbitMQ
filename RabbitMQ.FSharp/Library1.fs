876898f6c1482f43bdf442408b99bbc6abf80a7c
Erich Ess <erich.g.ess@gmail.com>
2014-03-07 08:15:26 -0800

    First Version of the Library.

--- RabbitMQ.FSharp/Library1.fs
+++ RabbitMQ.FSharp/Library1.fs
@@ -1,4 +1,27 @@
 ﻿namespace RabbitMQ.FSharp
 
-type Class1() = 
-    member this.X = "F#"
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+open System.Text
+
+module Client =
+    let readFromQueue (consumer:QueueingBasicConsumer) queueName =
+        let ea = consumer.Queue.Dequeue()
+        let body = ea.Body
+        Encoding.UTF8.GetString(body)
+
+    let publishToQueue (channel:IModel) queueName (message:string) =
+        let body = Encoding.UTF8.GetBytes(message)
+        channel.BasicPublish("", queueName, null, body)
+
+    let connectToRabbitMq address =
+        let factory = new ConnectionFactory(HostName = "localhost")
+        use connection = factory.CreateConnection()
+        use channel = connection.CreateModel()
+
+        fun queueName ->
+            channel.QueueDeclare( queueName, false, false, false, null ) |> ignore
+            let consumer = new QueueingBasicConsumer(channel) 
+            channel.BasicConsume(queueName, true, consumer) |> ignore
+
+            (fun () -> readFromQueue consumer queueName, publishToQueue channel queueName)

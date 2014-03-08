a327c1e53b1ca7ef252413bee1cd40be9b854153
Erich Ess <erich.g.ess@gmail.com>
2014-03-08 12:34:04 -0800

    code clean up.

--- Receiver/Program.fs
+++ Receiver/Program.fs
@@ -20,24 +20,6 @@ let main argv =
                         yield message
                 }
 
-    let validIds = [1; 2; 3; 4]
-    let idQuery = query{
-                    for id in validIds do
-                    select id
-                  }
-
-    // This will read all the messages coming in from RabbitMQ
-    // the messages have the format "id,text"
-    // this query extracts the id and then joins on the list of
-    // valid Ids.  So that only messages which have a valid id will
-    // be selected (the rest are all discarded).
-    let qQuery = query{
-                    for message in queue do
-                    let id,text = message.Split(',') |> (fun s -> (System.Int32.Parse(s.[0]), s.[1]))
-                    join validId in validIds on
-                        (id = validId)
-                    select (id,text)
-                 }
-    qQuery |> Seq.iter (printfn "%A")
+    queue |> Seq.iter (printfn "%A")
     printfn "%A" argv
     0 // return an integer exit code

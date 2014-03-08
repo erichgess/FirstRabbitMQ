41ae93ae0182c68f5ce9dda1223ab8dca2ee8c56
Erich Ess <erich.g.ess@gmail.com>
2014-03-08 12:29:06 -0800

    Fixed some bugs :(

--- RabbitMQ.FSharp/Client.fs
+++ RabbitMQ.FSharp/Client.fs
@@ -12,7 +12,9 @@ module Client =
         factory.CreateConnection()
 
     // I need to declare the type for connection because F# can't infer types on classes
-    let private openChannel (connection:IConnection) = connection.CreateModel()
+    let openChannel (connection:IConnection) = connection.CreateModel()
+
+    let declareQueue (channel:IModel) queueName = channel.QueueDeclare( queueName, false, false, false, null )
 
     let readFromQueue (consumer:QueueingBasicConsumer) queueName =
         let ea = consumer.Queue.Dequeue()
@@ -23,14 +25,11 @@ module Client =
         let body = Encoding.UTF8.GetBytes(message)
         channel.BasicPublish("", queueName, null, body)
 
-    let connectToQueue connection name =            // I don't have to declare the type here, because F# can infer the type from my call to openChannel
-        use channel = openChannel connection
+    let connectToQueue connection (channel:IModel) queueName =            // I don't have to declare the type of connection, because F# can infer the type from my call to openChannel
+        declareQueue channel queueName |> ignore
+        let consumer = new QueueingBasicConsumer(channel) 
+        channel.BasicConsume(queueName, true, consumer) |> ignore
 
-        fun queueName ->
-            channel.QueueDeclare( queueName, false, false, false, null ) |> ignore
-            let consumer = new QueueingBasicConsumer(channel) 
-            channel.BasicConsume(queueName, true, consumer) |> ignore
-
-            {Name = queueName; 
-            Read = (fun () -> readFromQueue consumer queueName); 
-            Publish = (publishToQueue channel queueName)}
+        {Name = queueName; 
+        Read = (fun () -> readFromQueue consumer queueName); 
+        Publish = (publishToQueue channel queueName)}
--- RabbitMQ/Program.fs
+++ RabbitMQ/Program.fs
@@ -2,23 +2,27 @@
 // See the 'F# Tutorial' project for more help.
 open RabbitMQ.Client
 open System.Text
+open RabbitMQ.FSharp.Client
 
 [<EntryPoint>]
 let main argv = 
-    let factory = new ConnectionFactory(HostName = "localhost")
-    use connection = factory.CreateConnection()
-    use channel = connection.CreateModel()
+//    let factory = new ConnectionFactory(HostName = "localhost")
+//    use connection = factory.CreateConnection()
+//    use channel = connection.CreateModel()
+//
+//    channel.QueueDeclare( "hello", false, false, false, null ) |> ignore
 
-    channel.QueueDeclare( "hello", false, false, false, null ) |> ignore
+    let connection = openConnection "localhost"
+    let channel = openChannel connection
+    let queue = (connectToQueue connection channel) "hello"
     
     let mutable i = 0
     while true do
         i <- i + 1
         let message = sprintf "%d,test" ((i + 1) % 10)  // send a message with a number from 0 to 9 along with some text
         printfn "Sending: %s" message
-        let body = Encoding.UTF8.GetBytes(message)
-        channel.BasicPublish("", "hello", null, body)
-        System.Threading.Thread.Sleep(2000)
+        queue.Publish message
+        System.Threading.Thread.Sleep(1000)
 
     printfn "%A" argv
     0 // return an integer exit code

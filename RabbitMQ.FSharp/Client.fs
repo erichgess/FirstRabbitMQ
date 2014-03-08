417b0fa071bb21fc14299d06c31553aa7e9c0c6b
Erich Ess <erich.g.ess@gmail.com>
2014-03-08 14:25:19 -0800

    code clean up.

--- RabbitMQ.FSharp/Client.fs
+++ RabbitMQ.FSharp/Client.fs
@@ -16,23 +16,21 @@ module Client =
 
     let declareQueue (channel:IModel) queueName = channel.QueueDeclare( queueName, false, false, false, null )
 
-    let readFromQueue (channel:IModel) queueName =
-        fun () -> 
-            let ea = channel.BasicGet(queueName, true)
-            if ea <> null then
-                let body = ea.Body
-                let message = Encoding.UTF8.GetString(body)
-                Some message
-            else
-                None
-
     let publishToQueue (channel:IModel) queueName (message:string) =
         let body = Encoding.UTF8.GetBytes(message)
         channel.BasicPublish("", queueName, null, body)
         
     let createQueueReader channel queue = 
         declareQueue channel queue |> ignore
-        readFromQueue channel queue
+        
+        fun () -> 
+            let ea = channel.BasicGet(queue, true)
+            if ea <> null then
+                let body = ea.Body
+                let message = Encoding.UTF8.GetString(body)
+                Some message
+            else
+                None
 
     let createQueueWriter channel queue =
         declareQueue channel queue |> ignore
